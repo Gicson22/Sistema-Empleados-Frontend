d2a9bcf9de8faab1e82479d11f953472cdbff1bb
Gicson22 <grivas@gmail.com>
2023-05-07 21:36:11 -0500

    Sistema de empleados Frontend

---
+++ src/app/registrar-empleado/registrar-empleado.component.ts
@@ -0,0 +1,40 @@
+import { EmpleadoService } from './../empleado.service';
+import { Empleado } from './../empleado';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-registrar-empleado',
+  templateUrl: './registrar-empleado.component.html',
+  styleUrls: ['./registrar-empleado.component.css']
+})
+export class RegistrarEmpleadoComponent implements OnInit {
+
+  empleado : Empleado = new Empleado();
+  constructor(private empleadoServicio:EmpleadoService,private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  guardarEmpleado(){
+    this.empleadoServicio.registrarEmpleado(this.empleado)
+    .subscribe(
+      {
+        next: dato => {
+          console.log(dato);
+          this.irALaListaDeEmpleados();
+      },
+      error: err => console.log(err.error.msg)
+      })
+    }
+
+  irALaListaDeEmpleados(){
+    this.router.navigate(['/empleados']);
+    swal('Empleado registrado',`El empleado ${this.empleado.nombre} ha sido registrado con exito`,`success`);
+  }
+
+  onSubmit(){
+    this.guardarEmpleado();
+  }
+}
